697a4b0ed95facb8536abe07268f99f57a08cafb
ogioldat <tomasz.ogiolda00@gmail.com>
2024-10-01 22:32:47 +0200

    chore: new cfg

--- dot_config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require'nvim-treesitter.configs'.setup 
-  ensure_installed = { "rust", "lua", "vim", "typescript", "c", "json", "yaml", "javascript", "markdown", "markdown_inline" },
-  highlight = {
-    enable = true,
-  },
-}
